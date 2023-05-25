ada1c5d5063a20de7e9be9b0f7bfe4ce2675813f
Ajayduddi <ajayprofessional19@gmail.com>
2023-05-25 23:38:33 +0530

    update js

--- Temperature converter/convert.js
+++ Temperature converter/convert.js
@@ -4,22 +4,22 @@ function convert(){
     if(type=='celsius'){
         let f=(temperature*(9/5)+32).toFixed(2);
         let k=(temperature+(273.15)).toFixed(2);
-        document.getElementById("value1").innerHTML=("temperature in kelvin - "+k);
-        document.getElementById("value2").innerHTML=("temperature in fahrenheit - "+f);
+        document.getElementById("value1").innerHTML=("temperature in kelvin  "+k);
+        document.getElementById("value2").innerHTML=("temperature in fahrenheit  "+f);
     }
     else if(type=='kelvin'){
         let c=(temperature-(273.15)).toFixed(2);
         let f=(temperature*(9/5)-459.67).toFixed(2);
 
-        document.getElementById("value1").innerHTML=("temperature in celsius - "+c);
-        document.getElementById("value2").innerHTML=("temperature in fahrenheit - "+f);
+        document.getElementById("value1").innerHTML=("temperature in celsius  "+c);
+        document.getElementById("value2").innerHTML=("temperature in fahrenheit  "+f);
     }
     else{
         let c=((temperature-32)*(5/9)).toFixed(2);
         let k=((temperature+459.67)*(5/9)).toFixed(2);
 
-        document.getElementById("value1").innerHTML=("temperature in kelvin - "+k);
-        document.getElementById("value2").innerHTML=("temperature in celsius - "+c);
+        document.getElementById("value1").innerHTML=("temperature in kelvin  "+k);
+        document.getElementById("value2").innerHTML=("temperature in celsius  "+c);
     }
 }
 
